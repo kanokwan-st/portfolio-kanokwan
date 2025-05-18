e2bc9316228d110e70bcf1fa15d45ad489b62ff3
kanokwan-st <ganokwn123@gmail.com>
2025-05-19 01:23:49 +0700

    feat(app.jsx): fix layout and add animation to each section

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import ButtonRound from './component/ButtonRound';
 
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 
 function App() {
   gsap.registerPlugin(TextPlugin);
   const animatedText = useRef(null);
   const wavedHand = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+  const aboutRef = useRef(null);
+  const experienceRef = useRef(null);
+  const skillBoxRef = useRef(null);
+  const projectRef = useRef(null);
 
   useEffect(() => {
     // Text typing animation
@@ -28,13 +35,69 @@ function App() {
       transformOrigin: 'bottom center',
       ease: 'power1.inOut'
     })
+    
+    // About section popup when scroll to
+    gsap.to(aboutRef.current, {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+      duration: 1,
+      ease: 'power3.out',
+      scrollTrigger: {
+        trigger: aboutRef.current,
+        start: "top 80%",
+        toggleActions: 'play reverse play reverse',
+      },
+    });
+
+    // Experience popup when scroll to
+    gsap.to(experienceRef.current, {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+      duration: 1,
+      ease: 'power3.out',
+      scrollTrigger: {
+        trigger: experienceRef.current,
+        start: "top 80%",
+        toggleActions: 'play reverse play reverse',
+      },
+    });
+
+    // Skill box popup when scroll to
+    gsap.to(skillBoxRef.current, {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+      duration: 1,
+      ease: 'power3.out',
+      scrollTrigger: {
+        trigger: skillBoxRef.current,
+        start: "top 80%",
+        toggleActions: 'play reverse play reverse',
+      },
+    });
+
+    // Project popup when scroll to
+    gsap.to(projectRef.current, {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+      duration: 1,
+      ease: 'power3.out',
+      scrollTrigger: {
+        trigger: projectRef.current,
+        start: "top 80%",
+        toggleActions: 'play reverse play reverse',
+      },
+    });
 
   }, [])
 
   return (
     <div className="relative min-h-screen bg-[#0E100F] text-[#FEFBE0]">
       {/* NAVBAR */}
-      <nav className="sticky z-20 top-0  py-5 px-32 bg-[#0E100F]">
+      <nav className="sticky z-20 top-0 py-5 px-32 bg-[#0E100F]">
         <div className='relative flex justify-between'>
           <div className="flex gap-4">
             <h1 className="text-lg font-medium">Kanokwan S.</h1>
@@ -45,7 +108,7 @@ function App() {
               <img src="/images/linkedin.png" alt="" className="h-8" />
             </a>
           </div>
-          <div className="hidden lg:flex gap-16 font-medium">
+          <div className="hidden lg:flex items-center gap-16 font-medium">
             <a href="#home" className='transform hover:scale-110 transition-transform duration-300'>
               Home</a>
             <a href="#about" className='transform hover:scale-110 transition-transform duration-300'>
@@ -56,28 +119,29 @@ function App() {
               Skills</a>
             <a href="#project" className='transform hover:scale-110 transition-transform duration-300'>
               Project</a>
-            <a href="#miniproject" className='transform hover:scale-110 transition-transform duration-300'>
+            <a href="#miniproject" className='hidden transform hover:scale-110 transition-transform duration-300'>
               Mini Project</a>
             <a href="#contact" className='transform hover:scale-110 transition-transform duration-300'>
               Contact</a>
           </div>
         </div>
         <hr className="absolute bottom-0 right-19 h-[0.5px] w-[90%] bg-[#3B3C37] border-none mx-auto" />
-
       </nav>
 
       {/* SECTION 1 */}
-      <section id="home" className="w-full h-130 px-32">
-        <article className="flex w-full h-[80%]">
+      <section id="home" className="flex flex-col justify-between w-full h-screen px-32">
+        <article className="flex w-full h-[70%]">
           <div className="flex flex-col justify-center w-[60%] h-full">
             <h2 className="text-4xl font-bold mb-5">Hello <span ref={wavedHand} className='inline-block align-text-top'>👋</span></h2>
             <h2 className="text-3xl font-bold">
-              I’m <span className="text-violet-400">KANOKWAN SAE-TAE</span>, a
+              I'm <span className="text-violet-400">KANOKWAN SAE-TAE</span>, a
               software Developer
             </h2>
             <div className='relative'>
               {/* animated text */}
               <h2 ref={animatedText} className="absolute pt-10 top-0 left-0 text-2xl font-bold"></h2>
+              {/* reserve space */}
+              <h2 className="pt-10 invisible text-2xl font-bold">I love logical thinking and planning.</h2>
             </div>
           </div>
           <figure className="w-[40%] h-full">
@@ -88,37 +152,42 @@ function App() {
             />
           </figure>
         </article>
-        <div className="flex justify-center gap-4 py-6">
-          <Button as='a' href="#project" className='px-10'>My Projects</Button>
-          <Button as='a' href="#experience" className='px-10'>Experience</Button>
+        <div className='h-[27%]'>
+          <div className="flex justify-center gap-8">
+            <Button as='a' href="#project" className='px-15'>My Projects</Button>
+            <Button as='a' href="#experience" className='px-15'>Experience</Button>
+          </div>
+
+        </div>
+      </section>
+      <hr className="mb-6 h-[0.5px] w-[90%] bg-[#3B3C37] border-none mx-auto" />
+
+      {/* SECTION 2 */}
+      <section ref={aboutRef} id="about" className="opacity-0 translate-y-10 scale-95 flex flex-col justify-center items-center px-64 py-25 w-full bg-[#2a1438]">
+        <h2 className="text-2xl font-bold pb-10">Know Me More</h2>
+        <div className='border-2 p-10 rounded-2xl bg-[#1E0F2F] shadow-2xl hover:scale-101 transition-all duration-300 will-change-transform'>
+          <p className="text-lg text-balance text-center">
+            I am a graduate of Generation’s Junior Software Developer Bootcamp
+            with a background in maintenance engineering, which has enhanced my
+            problem-solving and project management skills. I am eager to apply my
+            technical skills, and attention to detail in a Software Developer
+            role, contributing to growth and innovation. I am passionate about
+            continuous learning and excited to collaborate on projects that create
+            meaningful solutions.
+          </p>
         </div>
       </section>
       <hr className="m-6 h-[0.5px] w-[90%] bg-[#3B3C37] border-none mx-auto" />
 
-      {/* SECTION 2 */}
-      <section id="about" className="flex flex-col justify-center items-center px-64  w-full h-100 bg-[#2a1438]">
-        <h2 className="text-2xl font-bold pb-6">Know Me More</h2>
-        <p className="text-lg text-balance text-center">
-          I am a graduate of Generation’s Junior Software Developer Bootcamp
-          with a background in maintenance engineering, which has enhanced my
-          problem-solving and project management skills. I am eager to apply my
-          technical skills, and attention to detail in a Software Developer
-          role, contributing to growth and innovation. I am passionate about
-          continuous learning and excited to collaborate on projects that create
-          meaningful solutions.
-        </p>
-      </section>
-      <hr className="m-6 h-[0.5px] w-[90%] bg-[#3B3C37] border-none mx-auto" />
-
       {/* SECTION 3 */}
-      <section id="experience" className="flex flex-col justify-center items-center px-32 w-full h-150">
-        <h2 className="text-2xl font-bold pb-6">Experience</h2>
-          <Button as="a" href="/files/CV-Kanokwan.pdf" download>Download CV</Button>
-        <div className="flex flex-col pt-10">
+      <section id="experience" className="flex flex-col justify-center items-center px-32 py-15 w-full ">
+        <h2 className="text-2xl font-bold pb-8">Experience</h2>
+        <Button as="a" href="/files/CV-Kanokwan.pdf" download>Download CV</Button>
+        <div ref={experienceRef} className="opacity-0 translate-y-10 scale-95 flex flex-col pt-10">
           {/* Exp1 */}
           <div className="flex gap-2">
             <p>⚪</p>
-            <hr className="h-full w-1 bg-[#3B3C37] border-0" />
+            <hr className="h-auto w-1 bg-[#3B3C37] border-0" />
             <div>
               <h3 className='text-lg font-semibold'>Junior Software Developer Bootcamp</h3>
               <p>Feb 2025 - May 2025</p>
@@ -130,7 +199,7 @@ function App() {
           {/* Exp2 */}
           <div className="flex gap-2">
             <p>⚪</p>
-            <hr className="h-full w-1 bg-[#3B3C37] border-0" />
+            <hr className="h-auto w-1 bg-[#3B3C37] border-0" />
             <div>
               <h3 className='text-lg font-semibold'>Maintenance Engineer</h3>
               <p>Jan 2024 - Jan 2025</p>
@@ -143,7 +212,7 @@ function App() {
           {/* Exp3 */}
           <div className="flex gap-2">
             <p>⚪</p>
-            <hr className="h-full w-1 bg-[#3B3C37] border-0" />
+            <hr className="h-auto w-1 bg-[#3B3C37] border-0" />
             <div>
               <h3 className='text-lg font-semibold'>Compliant Robot Gripper</h3>
               <p>Aug 2021 - July 2022</p>
@@ -157,14 +226,14 @@ function App() {
       <hr className="m-6 h-[0.5px] w-[90%] bg-[#3B3C37] border-none mx-auto" />
 
       {/* SECTION 4 */}
-      <section id="skills" className="flex flex-col justify-center items-center lg:px-50 xl:px-80 py-4 w-full h-100 bg-[#2a1438]">
-        <h2 className="flex items-center justify-center w-full h-10 text-2xl font-bold">
+      <section id="skills" className="flex flex-col justify-center items-center lg:px-50 xl:px-80 py-20 w-full bg-[#2a1438]">
+        <h2 className="flex items-center justify-center pb-12 w-full h-10 text-2xl font-bold">
           Skill Set
         </h2>
-        <div className="w-full flex gap-10 p-8">
+        <div ref={skillBoxRef} className="opacity-0 translate-y-10 scale-95 w-full flex border-2 rounded">
           {/* BOX 1 */}
-          <div className="w-full flex flex-col items-center">
-            <h3 className="pb-4 text-xl font-semibold">Frontend</h3>
+          <div className="w-full flex flex-col items-center px-5 pt-7 pb-8 border-2">
+            <h3 className="pb-5 text-xl font-semibold">Frontend</h3>
             <div className="flex flex-wrap justify-center gap-2">
               <ButtonRound>HTML</ButtonRound>
               <ButtonRound>CSS</ButtonRound>
@@ -175,8 +244,8 @@ function App() {
             </div>
           </div>
           {/* BOX 2 */}
-          <div className="w-full flex flex-col items-center">
-            <h3 className="pb-4 text-xl font-semibold">Backend</h3>
+          <div className="w-full flex flex-col items-center px-5 pt-7 pb-8 border-2">
+            <h3 className="pb-5 text-xl font-semibold">Backend</h3>
             <div className="flex flex-wrap justify-center gap-2">
               <ButtonRound>Node.js</ButtonRound>
               <ButtonRound>SQLite</ButtonRound>
@@ -184,8 +253,8 @@ function App() {
             </div>
           </div>
           {/* BOX 3 */}
-          <div className="w-full flex flex-col items-center">
-            <h3 className="pb-4 text-xl font-semibold">Tools</h3>
+          <div className="w-full flex flex-col items-center px-5 pt-7 pb-8 border-2">
+            <h3 className="pb-5 text-xl font-semibold">Tools</h3>
             <div className="flex flex-wrap justify-center gap-2">
               <ButtonRound>Git</ButtonRound>
               <ButtonRound>VScode</ButtonRound>
@@ -196,19 +265,19 @@ function App() {
       <hr className="m-6 h-[0.5px] w-[90%] bg-[#3B3C37] border-none mx-auto" />
 
       {/* SECTION 5 */}
-      <section id="project" className="flex flex-col justify-center items-center px-32 py-10 w-full h-100">
-        <h2 className="flex items-center justify-center w-full h-10 text-2xl font-bold">
+      <section ref={projectRef} id="project" className="opacity-0 translate-y-10 scale-95 flex flex-col justify-center items-center px-32 py-10 w-full h-100">
+        <h2 className="flex items-center justify-center pb-12 w-full h-10 text-2xl font-bold">
           My Project
         </h2>
-        <article className="flex w-full h-full">
-          <figure className="flex items-center justify-center w-[50%] h-full bg-red-200">
+        <article className="flex gap-4 w-full h-full">
+          <figure className="flex items-center justify-center w-[50%] h-full bg-[#9557eb92] rounded-2xl">
             <img
               src="/images/webpic.png"
               alt="dev pic"
               className="w-[80%] h-[90%] object-contain"
             />
           </figure>
-          <div className="flex flex-col p-12 w-[50%] h-full bg-gray-700">
+          <div className="flex flex-col p-12 w-[50%] h-full bg-gray-700 rounded-2xl">
             <div className="flex items-center gap-8">
               <h3 className="pb-4 text-2xl font-semibold">Collectico</h3>
               <div className="flex items-center gap-1">
@@ -227,7 +296,7 @@ function App() {
       <hr className="m-6 h-[0.5px] w-[90%] bg-[#3B3C37] border-none mx-auto" />
 
       {/* SECTION 6 */}
-      <section id="miniproject" className="flex flex-col justify-center items-center px-32 w-full h-100 bg-[#2a1438]">
+      <section id="miniproject" className="hidden flex flex-col justify-center items-center px-32 w-full h-100 bg-[#2a1438]">
         <h2 className="flex items-center justify-center w-full h-10 pb-8 text-2xl font-bold">Mini Project</h2>
         <div className="grid grid-cols-3 gap-x-8 gap-y-4">
           <div className="w-20 h-20 bg-gray-400"></div>
@@ -250,7 +319,7 @@ function App() {
           <a href="https://linkedin.com/in/kanokwan-st" target="_blank">
             <img src="/images/linkedin-findme.png" alt="" className="h-14" />
           </a>
-          <a href="https://linkedin.com/in/kanokwan-st" target="_blank">
+          <a href="mailto:kanokwan.stae@gmail.com" target="_blank">
             <img src="/images/mail-findme.png" alt="" className="h-14" />
           </a>
         </div>
